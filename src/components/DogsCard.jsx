732b24a90e2e6d3c1702730c9300e08895f60d85
FarizaAlzh <a_fariza@kbtu.kz>
2025-10-10 00:50:42 +0500

    dog-card

--- src/components/DogsCard.jsx
+++ src/components/DogsCard.jsx
@@ -1,4 +1,12 @@
 // этот комп будет отвечать за отображения одной картинки
 import '../styles/DogsCard.css'
 
+function DogsCard ({url}) {
+    return (
+        <li className='dog-card'>
+            <img src={url} alt='random dog' loading='lazy'></img>
+        </li>
+    );
+}
+
 export default DogsCard;    
